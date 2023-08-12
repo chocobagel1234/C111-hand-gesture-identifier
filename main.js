e4e90c7d5c3ed56f2999f3f3198fa8c544ae9392
chocobagel1234 <118059248+chocobagel1234@users.noreply.github.com>
2023-08-12 12:31:42 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+prediction_1 = "";
+prediction_2 = "";
+
 Webcam.set({
     width:500,
     height:380,
@@ -21,4 +24,71 @@ classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models
 
 function modelLoaded() {
     console.log("Model Loaded!")
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data1 = "The first prediction is " + prediction_1;
+    speak_data2 = "And the second prediction is " + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data1 + speak_data2);
+    synth.speak(utterThis);
+}
+
+function check() {
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("gesture_result_1").innerHTML = results[0].label;
+        document.getElementById("gesture_result_2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        //Emoji codes:&#128400;(hello/bye-bye!), &#128077;(thumbs up), &#128078;(thumbs down), &#128076;(amazing!), &#9996;(victory) and &#128072;(pointing)
+        if(results[0].label == "Hello/Bye-Bye") {
+            document.getElementById("emoji_1").innerHTML = "<b>&#128400;</b>";
+        }
+        if(results[0].label == "Amazing") {
+            document.getElementById("emoji_1").innerHTML = "<b>&#128076;</b>";
+        }
+        if(results[0].label == "Victory") {
+            document.getElementById("emoji_1").innerHTML = "<b>&#9996;</b>";
+        }
+        if(results[0].label == "Pointing(to something)") {
+            document.getElementById("emoji_1").innerHTML = "<b>&#128072;</b>";
+        }
+        if(results[0].label == "Thumbs Up") {
+            document.getElementById("emoji_1").innerHTML = "<b>&#128077;</b>";
+        }
+        if(results[0].label == "Thumbs Down") {
+            document.getElementById("emoji_1").innerHTML = "<b>&#128076;</b>";
+        }
+
+        //line break - the following if() conditions are for results[1]
+        if(results[1].label == "Hello/Bye-Bye") {
+            document.getElementById("emoji_2").innerHTML = "<b>&#128400;</b>";
+        }
+        if(results[1].label == "Amazing") {
+            document.getElementById("emoji_2").innerHTML = "<b>&#128076;</b>";
+        }
+        if(results[1].label == "Victory") {
+            document.getElementById("emoji_2").innerHTML = "<b>&#9996;</b>";
+        }
+        if(results[1].label == "Pointing(to something)") {
+            document.getElementById("emoji_2").innerHTML = "<b>&#128072;</b>";
+        }
+        if(results[1].label == "Thumbs Up") {
+            document.getElementById("emoji_2").innerHTML = "<b>&#128077;</b>";
+        }
+        if(results[1].label == "Thumbs Down") {
+            document.getElementById("emoji_2").innerHTML = "<b>&#128076;</b>";
+        }
+        
+    }
 }
